c397a13951c5965689127f7c48325937e6ee57ce
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-06-20 17:52:08 -0400

    TODO: handle Save settings and detection

--- frontend/src/app/components/Settings/Settings.tsx
+++ frontend/src/app/components/Settings/Settings.tsx
@@ -3,22 +3,22 @@ import axios from 'axios';
 import * as React from 'react';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
 import Emitter from '../../utils/emitter';
+import { Page, PageSection, Text, TextContent, TextVariants, Form, FormGroup, Button, TextInput, TextInputGroup, TextInputGroupMain, TextInputGroupUtilities } from '@patternfly/react-core';
+import { EyeIcon } from '@patternfly/react-icons';
 
 interface SettingsProps { }
 
 class S3Settings {
-    access_key: string;
-    secret_key: string;
-    bucket_name: string;
+    accessKeyId: string;
+    secretAccessKey: string;
     region: string;
     endpoint: string;
 
-    constructor(access_key: string, secret_key: string, bucket_name: string, region: string, endpoint: string) {
-        this.access_key = access_key;
-        this.secret_key = secret_key;
-        this.bucket_name = bucket_name;
-        this.region = region;
-        this.endpoint = endpoint;
+    constructor(accessKeyId: string, secretAccessKey: string, region: string, endpoint: string) {
+        this.accessKeyId = accessKeyId ?? '';
+        this.secretAccessKey = secretAccessKey ?? '';
+        this.region = region ?? '';
+        this.endpoint = endpoint ?? '';
     }
 }
 
@@ -27,14 +27,28 @@ const Settings: React.FunctionComponent<SettingsProps> = () => {
     const location = useLocation();
     const params = useParams();
 
-    const [s3Settings, setS3Settings] = React.useState<S3Settings | null>(null);
+    const [s3Settings, setS3Settings] = React.useState<S3Settings>(new S3Settings('', '', '', ''));
+
+    const [showSecretKey, setShowSecretKey] = React.useState<boolean>(false);
+
+    const handleChange = (value, field) => {
+        setS3Settings(prevState => ({
+            ...prevState,
+            [field]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you would typically send the s3Settings to your server or some API endpoint
+    };
 
     React.useEffect(() => {
         axios.get(`${config.backend_api_url}/settings`)
             .then((response) => {
                 const { settings } = response.data;
                 if (settings !== undefined) {
-                    setS3Settings(new S3Settings(settings.accessKeyId, settings.secretAccessKey, settings.bucket_name, settings.region, settings.endpoint));
+                    setS3Settings(new S3Settings(settings.accessKeyId, settings.secretAccessKey, settings.region, settings.endpoint));
                 }
             })
             .catch((error) => {
@@ -44,14 +58,68 @@ const Settings: React.FunctionComponent<SettingsProps> = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Settings</h1>
-            <p>Access key: {s3Settings?.access_key}</p>
-            <p>Secret key: {s3Settings?.secret_key}</p>
-            <p>Bucket name: {s3Settings?.bucket_name}</p>
-            <p>Region: {s3Settings?.region}</p>
-            <p>Endpoint: {s3Settings?.endpoint}</p>
-        </div>
+        <Page className='buckets-list'>
+            <PageSection>
+                <TextContent>
+                    <Text component={TextVariants.h1}>Settings</Text>
+                </TextContent>
+            </PageSection>
+            <PageSection>
+                <Form onSubmit={handleSubmit}>
+                    <TextContent>
+                        <Text component={TextVariants.h2}>S3 Settings</Text>
+                    </TextContent>
+                    <FormGroup label="Access key" fieldId="accessKeyId">
+                        <TextInput
+                            value={s3Settings.accessKeyId}
+                            onChange={(_event, value) => handleChange(value, 'accessKeyId')}
+                            id="accessKeyId"
+                            name="accessKeyId"
+                            className='form-s3-settings'
+                        />
+                    </FormGroup>
+                    <FormGroup label="Secret key" fieldId="secretAccessKey">
+                        <TextInputGroup className='form-s3-settings'>
+                            <TextInputGroupMain
+                                value={s3Settings.secretAccessKey}
+                                onChange={(_event, value) => handleChange(value, 'secretAccessKey')}
+                                id="secretAccessKey"
+                                name="secretAccessKey"
+                                type={showSecretKey ? 'text' : 'password'}
+                            />
+                            <TextInputGroupUtilities>
+                                <Button
+                                    variant="plain"
+                                    aria-label={showSecretKey ? 'Hide secret key' : 'Show secret key'}
+                                    onClick={() => setShowSecretKey(!showSecretKey)}
+                                >
+                                    <EyeIcon />
+                                </Button>
+                            </TextInputGroupUtilities>
+                        </TextInputGroup>
+                    </FormGroup>
+                    <FormGroup label="Region" fieldId="region">
+                        <TextInput
+                            value={s3Settings.region}
+                            onChange={(_event, value) => handleChange(value, 'region')}
+                            id="region"
+                            name="region"
+                            className='form-s3-settings'
+                        />
+                    </FormGroup>
+                    <FormGroup label="Endpoint" fieldId="endpoint">
+                        <TextInput
+                            value={s3Settings.endpoint}
+                            onChange={(_event, value) => handleChange(value, 'endpoint')}
+                            id="endpoint"
+                            name="endpoint"
+                            className='form-s3-settings-long'
+                        />
+                    </FormGroup>
+                    <Button type="submit" className='form-s3-submit'>Save S3 Settings</Button>
+                </Form>
+            </PageSection>
+        </Page>
     );
 };
 
