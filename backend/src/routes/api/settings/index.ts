37ea78a3d8edca49a17b726c8b54aaf3fd2554ab
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-06-24 16:05:06 -0400

    backend

--- backend/src/routes/api/settings/index.ts
+++ backend/src/routes/api/settings/index.ts
@@ -1,3 +1,4 @@
+import { ListBucketsCommand } from '@aws-sdk/client-s3';
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 const config = require('../../../utils/config');
@@ -12,5 +13,27 @@ export default async (fastify: FastifyInstance): Promise<void> => {
       endpoint: endpoint,
     };
     reply.send({ settings });
+  });
+
+  fastify.post('/', async (req: FastifyRequest, reply: FastifyReply) => {
+    const { accessKeyId, secretAccessKey, region, endpoint } = req.body as any;
+    try {
+      config.setConfig(accessKeyId, secretAccessKey, region, endpoint);
+      reply.send({ message: 'Settings updated successfully' });
+    } catch (error) {
+      console.error('Error updating settings', error);
+      reply.code(500).send({ message: error });
+    }
+  });
+
+  fastify.get('/test', async (req: FastifyRequest, reply: FastifyReply) => {
+    const { s3Client } = config.getConfig();
+    try {
+      await s3Client.send(new ListBucketsCommand({}));
+      reply.send({ message: 'Connection successful' });
+    } catch (error) {
+      console.error('Error testing connection', error);
+      reply.code(500).send({ message: error });
+    }
   });
 };
